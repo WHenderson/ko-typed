2aff1d45b1c7ba301ccd3b244d29a30196d76d08
WHenderson <whenderson.github@gmail.com>
2015-12-30 17:49:53 +1100

    Remove fiddling

--- examples/fiddle.js
+++ examples/fiddle.js
@@ -3,18 +3,6 @@ var isAn = require('is-an');
 var util = require('util');
 require('../index')(ko);
 
-var base = ko.observable().extend({ type: 'Number' })
-var convert = base.extend({ convert: {
-  type: 'Number',
-  Number: {
-    Number: {
-      read: function (value) { throw new TypeError('read'); return value + 1; },
-      write: function (value) { throw new TypeError('write'); return value - 1; }
-    }
-  }
-}});
-
-base(42);
-console.log(convert());
-convert(42);
+var base = ko.observable().extend({ type: 'Number' });
+var convert = base.extend({ convert: true });
 
